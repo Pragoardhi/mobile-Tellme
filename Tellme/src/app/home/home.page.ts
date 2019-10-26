a98627b9ee275b7b66a8171b2c7b2209213fe779
IvanPranataGunawan <ivan.gunawan1@student.umn.ac.id>
2019-10-26 13:10:11 +0700

    ivan

--- Tellme/src/app/home/home.page.ts
+++ Tellme/src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+// import { google } from '@google/maps';
+import { Component, ViewChild, AfterContentInit, OnInit } from '@angular/core';
+// declare var google: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterContentInit{
+  map;
+  @ViewChild('mapElement', { static: true }) mapElement;
+  constructor() {} 
+  
+  ngOnInit() : void{ 
+  }
 
-  constructor() {}
+  ngAfterContentInit() : void{
+    this.map = new google.maps.Map(
+      this.mapElement.nativeElement,{
+        center: {lat: -34.397, lng: 150.664},
+        zoom: 8
+      })
+  } 
 
 }
